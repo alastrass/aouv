f1684b649ad08b44b96e53617f60c61ae2408581
alastrass <154005517+alastrass@users.noreply.github.com>
2025-09-04 04:09:48 +0200

    Updated useRemoteSync.ts

--- src/hooks/useRemoteSync.ts
+++ src/hooks/useRemoteSync.ts
@@ -1,10 +1,8 @@
-@@ .. @@
-   const generateSessionCode = useCallback(() => {
-     const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-     let result = '';
--    for (let i = 0; i < 6, i < 6; i++) {
-+    for (let i = 0; i < 6; i++) {
-       result += chars.charAt(Math.floor(Math.random() * chars.length));
-     }
-     return result;
-   }, []);
+const generateSessionCode = useCallback(() => {
+  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+  let result = '';
+  for (let i = 0; i < 6; i++) {
+    result += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+  return result;
+}, []);
