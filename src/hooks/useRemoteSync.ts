bf15a3a18c309372a5da70aebb8cd1ae5e92d857
alastrass <154005517+alastrass@users.noreply.github.com>
2025-12-11 23:31:18 +0100

    Deleted types.ts

--- src/hooks/useRemoteSync.ts
+++
@@ -1,8 +0,0 @@
-const generateSessionCode = useCallback(() => {
-  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-  let result = '';
-  for (let i = 0; i < 6; i++) {
-    result += chars.charAt(Math.floor(Math.random() * chars.length));
-  }
-  return result;
-}, []);
